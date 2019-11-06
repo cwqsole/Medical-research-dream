1badca65f3f17a047bc04feadca249ae73863ebd
hukanglong <2877203930@qq.com>
2019-11-06 10:30:21 +0800

    2019.11.06 胡康龙

--- MedicalMall2/app/src/main/java/com/example/usercwq/medicalmall/mvp/view/model/CurriculumModel.java
+++
@@ -1,9 +0,0 @@
-package com.example.usercwq.medicalmall.model;
-
-import com.example.usercwq.medicalmall.base.BaseModel;
-
-public class CurriculumModel extends BaseModel {
-    public void getData() {
-
-    }
-}
